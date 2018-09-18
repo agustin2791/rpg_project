39ad44ddaa1e00b6c8330ff3b4b97f483376b1b5
Jesus Diaz-Barriga <jagustin2791@gmail.com>
2018-09-18 01:07:09 -0700

    character level import

--- pyRPG/app/management/commands/import_char_level.py
+++ pyRPG/app/management/commands/import_char_level.py
@@ -13,53 +13,79 @@ class Command(BaseCommand):
 
     @staticmethod
     def level_file():
-        level_class = glob.glob(os.path.join(os.path.join(settings.BASE_DIR, 'file/JSON/character_classes'), '*.json'))
+        home_dir = os.path.join(settings.BASE_DIR, 'files/JSON/character_classes')
+        level_class = glob.glob(os.path.join(home_dir, '*.json'))
+        print level_class
         return level_class
 
     @staticmethod
     def class_level(index, lvl):
-        for feat in level['levels']:
-            if lvl['rage'] != 'NaN':
-                rage = lvl['rage']
-            else:
+        for feat in lvl['levels']:
+            try:
+                if feat['rage'] != 'NaN':
+                    rage = feat['rage']
+            except:
                 rage = None
-            if lvl['rage_dmg'] != 'NaN':
-                rage_dmg = lvl['rage_dmg']
-            else:
-                rage = None
-            if lvl['cantrips'] != 'NaN':
-                cantrips = lvl['cantrips']
-            else:
+            try:
+                if feat['rageDamage'] != 'NaN':
+                    rage_dmg = feat['rageDamage']
+            except:
+                rage_dmg = None
+            try:
+                if feat['cantrips'] != 'NaN':
+                    cantrips = feat['cantrips']
+            except:
                 cantrips = None
-            if lvl['spells'] != 'NaN':
-                spells = lvl['spells']
-            else:
+            try:
+                if feat['spells'] != 'NaN':
+                    spells = feat['spells']
+            except:
                 spells = None
-            if lvl['martial_art'] != 'NaN':
-                martial_art = lvl['martial_art']
-            else:
+            try:
+                if feat['martial_art'] != 'NaN':
+                    martial_art = feat['martial_art']
+            except:
                 martial_art = None
-            if lvl['ki_points'] != 'NaN':
-                ki_points = lvl['ki_points']
-            else:
+            try:
+                if feat['ki_points'] != 'NaN':
+                    ki_points = feat['ki_points']
+            except:
                 ki_points = None
-            if lvl['unarmored_mvm'] != 'NaN':
-                unarmored_mvm = lvl['unarmored_mvm']
-            else:
+            try:
+                if feat['unarmored_mvm'] != 'NaN':
+                    unarmored_mvm = feat['unarmored_mvm']
+            except:
                 unarmored_mvm = None
-            if lvl['sneak'] != 'NaN':
-                sneak = lvl['sneak']
-            else:
+            try:
+                if feat['sneak'] != 'NaN':
+                    sneak = feat['sneak']
+            except:
                 sneak = None
-            if lvl['sorcery_points'] != 'NaN':
-                sorcery_points = lvl['sorcery_points']
-            else:
+            try:
+                if feat['sorcery_points'] != 'NaN':
+                    sorcery_points = feat['sorcery_points']
+            except:
                 sorcery_points = None
-            if lvl['invocations'] != 'NaN':
-                invocations = lvl['invocations']
-            else:
+            try:
+                if feat['invocations'] != 'NaN':
+                    invocations = feat['invocations']
+            except:
                 invocations = None
-            if lvl['spell_slots'] != 'NaN':
-                spell_slots = lvl['spell_slots']
-            else:
+            try:
+                if feat['spell_slots'] != 'NaN':
+                    spell_slots = feat['spell_slots']
+            except:
                 spell_slots = None
+            print rage, rage_dmg, cantrips, spells, martial_art, ki_points, unarmored_mvm, sneak, sorcery_points, invocations, spell_slots
+
+    def handle(self, *args, **options):
+        start_time = dt.datetime.now()
+        class_lvl = self.level_file()
+
+        print '--------Start--------'
+        for index, f in enumerate(class_lvl):
+            print f
+            df = json.load(open(f, 'r'))
+            self.class_level(index, df)
+            print(dt.datetime.now() - start_time)
+        print '--------End----------'
