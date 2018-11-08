9dedbd78e7b2e07a7e094a0793ab01fd7832d99c
Jesus Diaz-Barriga <jagustin2791@gmail.com>
2018-11-07 17:13:12 -0800

    edit function

--- pyRPG/static/js/character/character_info.js
+++ pyRPG/static/js/character/character_info.js
@@ -12,7 +12,15 @@ var submit_trait = function (url, csrf, trait, description) {
             'description': description
         },
         success: function (data) {
-            $('.character_traits').empty().html(data);
+            if (trait == 'background') {
+                $('.character_background').empty().html(data);
+            }
+            else if (trait.startsWith('feature')) {
+                $('.character_feature').empty().html(data);
+            }
+            else {
+                $('.character_traits').empty().html(data);
+            }
             getInfo();
         }
     });
@@ -20,6 +28,7 @@ var submit_trait = function (url, csrf, trait, description) {
 // edit button
 var getInfo = function () {
     var info = document.getElementsByClassName('info-div');
+    console.log(info.length);
     for (var i = 0; i < info.length; i++) {
         info[i].addEventListener('mouseenter', function (i) {
             var update = i.path[0].dataset.update;
@@ -38,7 +47,11 @@ var getInfo = function () {
     }
 };
 var openEdit = function (update, text) {
-    var html = "\n  <form class=\"" + update + "_form\" data-update=\"" + update + "\">\n    <div class=\"form-group\">\n      <textarea name=\"" + update + "\" class=\"form-control\" rows=\"5\" cols=\"80\">" + text + "</textarea>\n    </div>\n    <button type=\"submit\" class=\"btn btn-primary\" name=\"button\">Submit</button> <button type=\"button\" class=\"btn cancel\" data-update=\"" + update + "\">Cancel</button>\n  </form>";
+    var toUpdate = update;
+    if (toUpdate.startsWith('feature')) {
+        toUpdate = 'feature';
+    }
+    var html = "\n  <form class=\"" + toUpdate + "_form\" data-update=\"" + update + "\">\n    <div class=\"form-group\">\n      <textarea name=\"" + update + "\" class=\"form-control\" rows=\"5\" cols=\"80\">" + text + "</textarea>\n    </div>\n    <button type=\"submit\" class=\"btn btn-primary\" name=\"button\">Submit</button> <button type=\"button\" class=\"btn cancel\" data-update=\"" + update + "\">Cancel</button>\n  </form>";
     $('.info-text-edit-' + update).empty().html(html);
     document.getElementsByClassName('edit_' + update)[0].style.visibility = 'hidden';
     getInfo();
